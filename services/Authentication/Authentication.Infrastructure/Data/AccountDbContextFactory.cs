1f7dbb9681501830be6502c7cfd076aa11189d18
duynp <duynp@aiontech.vn>
2025-10-21 20:22:53 +0700

    update

--- services/Authentication/Authentication.Infrastructure/Data/AccountDbContextFactory.cs
+++ services/Authentication/Authentication.Infrastructure/Data/AccountDbContextFactory.cs
@@ -1,19 +1,27 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Authentication.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
 
-namespace Authentication.Infrastructure.Data
+namespace Authentication.Infrastructure
 {
     public class AccountDbContextFactory : IDesignTimeDbContextFactory<AccountDbContext>
     {
         public AccountDbContext CreateDbContext(string[] args)
         {
-            // Get API folder from Infrastructure folder
-            var basePath = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory())!.FullName, "API");
+            // Xác định môi trường hiện tại (nếu không có thì mặc định là Development)
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development";
 
-            IConfigurationRoot configuration = new ConfigurationBuilder()
+            // Tìm đường dẫn tới API project (vì appsettings nằm ở đó)
+            var basePath = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory())!.FullName, "Authentication.API");
+
+            // Build configuration theo môi trường
+            var configuration = new ConfigurationBuilder()
                 .SetBasePath(basePath)
                 .AddJsonFile("appsettings.json", optional: false)
+                .AddJsonFile($"appsettings.{environment}.json", optional: true)
                 .Build();
 
             var optionsBuilder = new DbContextOptionsBuilder<AccountDbContext>();
