00b7558999ba297cc17157b692d561613d5f5e00
duynp <duynp@aiontech.vn>
2025-10-23 09:35:35 +0700

    update

--- Gateway.Api/Program.cs
+++ Gateway.Api/Program.cs
@@ -2,29 +2,46 @@
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+
 // Đọc config ReverseProxy từ appsettings.json
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
+builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // bật swagger
+//if (app.Environment.IsDevelopment() || app.Environment.IsStaging())
+//{
+//    //app.UseSwagger();
+//    app.UseSwaggerUI(c =>
+//    {
+//        if (builder.Environment.IsDevelopment())
+//        {
+//            c.SwaggerEndpoint("/swagger/v1/swagger.json", "Gateway API");
+//            c.RoutePrefix = string.Empty;
+//        }
+//        else
+//        {
+//            c.SwaggerEndpoint("/auth/swagger/v1/swagger.json", "Authentication API");
+//            c.SwaggerEndpoint("/course/swagger/v1/swagger.json", "Course API");
+//            c.RoutePrefix = "gateway";
+//        }
+//    });
+//}
+
 if (app.Environment.IsDevelopment() || app.Environment.IsStaging())
 {
-    app.UseSwagger();
     app.UseSwaggerUI(c =>
     {
-        if (builder.Environment.IsDevelopment())
-        {
-            c.SwaggerEndpoint("/swagger/v1/swagger.json", "Gateway API");
-            c.RoutePrefix = string.Empty;
-        }
-        else
-        {
-            c.SwaggerEndpoint("/auth/swagger/v1/swagger.json", "Authentication API");
-            c.SwaggerEndpoint("/course/swagger/v1/swagger.json", "Course API");
-            c.RoutePrefix = "gateway";
-        }
+        // Các swagger document của service
+        c.SwaggerEndpoint("/auth/swagger/v1/swagger.json", "Authentication API");
+        c.SwaggerEndpoint("/course/swagger/v1/swagger.json", "Course API");
+
+        // Giao diện Swagger UI sẽ hiển thị tại /gateway
+        c.RoutePrefix = "gateway";
     });
 }
 
